7c072375ae738a91dadbefe8630f130841626b37
srikar-eranky <srikar.eranky@gmail.com>
2023-11-02 18:31:01 -0700

    week 4

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -1,5 +1,36 @@
 
+const getGame = async (req, res) => {
+    const game = {
+        sentence: "Hello there",
+        correctCharacters: 14,
+        incorrectCharacters: 15,
+        wpm: 100,
+        time: 13.33
+    };
+
+    res.status(200).json(game);
+}
+
+const postGame = async (req, res) => {
+    const message = req.body;
+    const sentence = message.sentence;
+    const correctCharacters = message.correctCharacters;
+    const incorrectCharacters = message.incorrectCharacters;
+    const wpm = message.wpm;
+    const time = message.time;
+    
+    if(!sentence || !correctCharacters || !incorrectCharacters || !wpm || !time){
+        res.status(400).json({ error: "invalid request"});
+        console.log(message)
+        return 
+    }
+    if(sentence.length != correctCharacters + incorrectCharacters){
+        res.status(400).json({ error: "Sentence length is incorrect"});
+        return
+        
+    }
+    res.status(200).json(message);
+}
 
 
-
-module.exports = {  };
+module.exports = { getGame, postGame };
